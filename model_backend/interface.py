3fe19544d0a1424a2dd6d482441759271fc2cc61
Kartikey Subramanium <kartikey@tutanota.com>
2024-04-12 12:23:18 +0530

    Modify Audio.py

--- model_backend/interface.py
+++ model_backend/interface.py
@@ -1,14 +1,25 @@
 import random
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
+CONTEXT = """You are a friendly home assistant.
+
+
+QUESTION: {question}
+==============
+{summaries}
+==============
+FINAL ANSWER IN ENGLISH:"""
+
 class Model:
     def __init__(self, name: str = "google/flan-t5-base") -> None:
         self.name = name
         self.tokenizer = T5Tokenizer.from_pretrained(self.name)
         self.model = T5ForConditionalGeneration.from_pretrained(self.name)
+        self.context = CONTEXT
 
     def infer(self, message: str) -> str:
-        input_ids = self.tokenizer(message, return_tensors="pt").input_ids
+        input_ids = self.tokenizer(
+                self.context + message, return_tensors="pt").input_ids
         outputs = self.model.generate(input_ids)
 
         return self.tokenizer.decode(outputs[0])
@@ -17,4 +28,4 @@ class Model:
         self.context = context
 
     def context(self) -> str:
-        return ""
+        return self.context
